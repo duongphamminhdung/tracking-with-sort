7b3de9903aa9c0e9196d22b3ac0a6a1e85ea12d2
duongphamminhdung <duongphamminhdung@gmail.com>
2023-10-14 17:58:07 +0000

    update for detections

--- tracking-with-deep-sort.py
+++ tracking-with-deep-sort.py
@@ -18,6 +18,7 @@ from PIL import Image
 from deep_sort import nn_matching
 from deep_sort.detection import Detection
 from deep_sort.tracker import Tracker
+from utils import generate_detections
 
 config_path='config/yolov3.cfg'
 weights_path='config/yolov3.weights'
@@ -35,6 +36,9 @@ model.eval()
 classes = utils.load_classes(class_path)
 Tensor = torch.cuda.FloatTensor
 
+#for detections
+detection_model_path = '/root/tracking-with-sort/config/mars-small128.pb'
+encoder = generate_detections.create_box_encoder(detection_model_path, batch_size=32)
 def detect_image(img):
     # scale and pad image
     ratio = min(img_size/img.size[0], img_size/img.size[1])
@@ -52,38 +56,60 @@ def detect_image(img):
     # run inference on the model and get detections
     with torch.no_grad():
         detections = model(input_img)
-        detections = utils.non_max_suppression(detections, 80, conf_thres, nms_thres)\
+        detections = utils.non_max_suppression(detections, 80, conf_thres, nms_thres)
     detection_list = []
     for row in detections[0]:
         bbox, confidence, feature = row[0:4], row[4], row[10:]
-        if bbox[3] < min_height:
+        bgr_image = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        feature = encoder(bgr_image, row[0:4].copy())
+        if bbox[3] < 0: #min_height
             continue
         detection_list.append(Detection(bbox, confidence, feature))
-    return detections[0]
+    return detection_list
 
 cmap = plt.get_cmap('tab20b')
 colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
 
-mot_tracker = Sort()
+#for tracker
+metric = nn_matching.NearestNeighborDistanceMetric(
+    "cosine", max_cosine_distance, nn_budget)
+mot_tracker = Tracker(metric)
 
-#while(True):
-def detector(frame):
+
+def detector(frame, min_confidence=0.6):
     # ret, frame = vid.read()
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     pilimg = Image.fromarray(frame)
     detections = detect_image(pilimg)
+    
+    detections = [d for d in detections if d.confidence >= min_confidence]
 
+    # Run non-maxima suppression.
+    boxes = np.array([d.tlwh for d in detections])
+    scores = np.array([d.confidence for d in detections])
+    indices = preprocessing.non_max_suppression(
+        boxes, nms_max_overlap, scores)
+    detections = [detections[i] for i in indices]
+    
     img = np.array(pilimg)
     pad_x = max(img.shape[0] - img.shape[1], 0) * (img_size / max(img.shape))
     pad_y = max(img.shape[1] - img.shape[0], 0) * (img_size / max(img.shape))
     unpad_h = img_size - pad_y
     unpad_w = img_size - pad_x
     if detections is not None:
-        tracked_objects = mot_tracker.update(detections.cpu())
+        tracked_objects = []
+        tracker.predict()
+        tracker.update(detections)
+        for track in tracker.tracks:
+            if not track.is_confirmed() or track.time_since_update > 1:
+                continue
+            bbox = track.to_tlwh()
+            tracked_objects.append([
+                bbox[0], bbox[1], bbox[2], bbox[3], track.track_id])
 
         unique_labels = detections[:, -1].cpu().unique()
         n_cls_preds = len(unique_labels)
-        for x1, y1, x2, y2, obj_id, cls_pred in tracked_objects:
+        for x1, y1, x2, y2, obj_id in tracked_objects:
             box_h = int(((y2 - y1) / unpad_h) * img.shape[0])
             box_w = int(((x2 - x1) / unpad_w) * img.shape[1])
             y1 = int(((y1 - pad_y // 2) / unpad_h) * img.shape[0])
@@ -91,18 +117,11 @@ def detector(frame):
 
             color = colors[int(obj_id) % len(colors)]
             color = [i * 255 for i in color]
-            cls = classes[int(cls_pred)]
             cv2.rectangle(frame, (x1, y1), (x1+box_w, y1+box_h), color, 4)
             cv2.rectangle(frame, (x1, y1-35), (x1+len(cls)*19+60, y1), color, -1)
-            cv2.putText(frame, cls + "-" + str(int(obj_id)), (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 3)
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
     return frame
 
-    # fig=figure(figsize=(12, 8))
-    # title("Video Stream")
-    # imshow(frame)
-    # show()
-    # clear_output(wait=True)
     
 import cv2
 cap = cv2.VideoCapture('/root/tracking-with-sort/MOT16-04-raw.webm')
